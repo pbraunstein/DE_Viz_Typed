f4dd7af8514ea234a973a179668221837e48c23d
pbraunstein <pbraunstein12@gmail.com>
2016-02-13 23:28:12 -0500

    Acquired d3.d.ts file to make d3 work in TypeScript. Started creating a new object

--- verb_viz.ts
+++ verb_viz.ts
@@ -1,3 +1,4 @@
+/// <reference path="lib/d3.d.ts"/>
 abstract class Word {
 	constructor(public german: string, public english: string) {}
 }
@@ -23,3 +24,31 @@ class RootWord extends Word {
 		return this.children;
 	}
 }
+
+// Takes raw json from data.json
+class Dictionary {
+	private roots: RootWord[];
+	constructor(input_json: any) {
+		this.roots = [];
+		console.log(this.build_root(input_json[0]));
+	}
+
+	build_root(raw_root: any) {
+		var new_root = new RootWord(raw_root.root, raw_root.trans);
+
+		
+		
+		return new_root;
+
+	}
+}
+
+function run() {
+	d3.json("data/data.json", function(error, json) {
+		if (error) {return console.warn(error);}
+		
+		var german_words = new Dictionary(json);
+	})
+}
+
+run();
